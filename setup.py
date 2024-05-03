b3c0dd092194b349bd2c18846c67ff4b77b645d6
paco-pastor <pacopas1512@gmail.com>
2024-05-03 22:43:11 +0200

    🔨 Setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+   name="candle",
+   version="0.1.0",
+   description="🕯️ Minimal python logger with emojis",
+   author="Paco Pastor",
+   author_email="foomail@foo.example",
+   url="https://github.com/paco-pastor/candle",
+   packages=setuptools.find_packages(),
+)
