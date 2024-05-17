855b778acdd329ecff8fd797b8e0cd137f4ce69e
Paco-Pastor <paco.pastor@hippolyte-rh.fr>
2024-05-17 13:34:10 +0200

    ✨ Decorator for classes

--- src/decorator.py
+++ src/decorator.py
@@ -1,75 +1,42 @@
 from main import Candle
 
 # TODO raise exception with personnalized logs, dont continue the program
-# FIXME MaClasse() takes no arguments
 
 
-def candle(func):
-    def candle_func(*args, **kwargs):
-        try:
-            print("RETURN")
-            return func(*args, **kwargs)
-        except Exception as e:
-            print("EXCEPTION")
-            # raise # for debug
-            Candle().error(f"Erreur dans {func.__name__}" + str(e))
-
-    if isinstance(func, type):
-        print("IS CLASS")
-        class CandleClass(entity):
-            def __init__(self, *args, **kwargs):
-                super().__init__(*args, **kwargs)
-                print("\nCLASSE AVEC DECORATEUR\n")
-
-            def __getattribute__(self, name):
-                print(name)
-                obj = super().__getattribute__(name)
-                if callable(obj):
-                    print("Méthode décorée appelée")
-                    # obj = candle_func(obj)
-                    # setattr(self, name, candle_func(obj))
-                return candle_func(obj)
+def candle(entity):
+    if isinstance(entity, type):
         
-        return CandleClass
-        
-    else:
-        return candle_func
+        def wrapper(entity):
+            for attr in entity.__dict__:
+                if callable(getattr(entity, attr)):
+                    setattr(entity, attr, candle(getattr(entity, attr)))
+            return entity
+        return wrapper(entity)
+    
+    if not isinstance(entity, type):
+        def wrapper(*args, **kwargs):
+            try:
+                return entity(*args, **kwargs)
+            except Exception as e:
+                # raise # for debug
+                Candle().error(f"Erreur dans {entity.__name__}" + str(e))
+        return wrapper
+
 
 
 # Exemple d'utilisation du décorateur avec une fonction
-# @candle
-# def ma_fonction():
-#     a = []
-#     print(a[1])
-#     print("Ma fonction originale")
+@candle
+def ma_fonction():
+    a = []
+    print(a[1])
 
-# ma_fonction()
-
-
-def print_decorator(func):
-    def wrapper(*args, **kwargs):
-        print("-------------------------")
-        return func(*args, **kwargs)
-    return wrapper
-
-def for_all_methods(decorator):
-    def decorate(cls):
-        for attr in cls.__dict__: # there's propably a better way to do this
-            if callable(getattr(cls, attr)):
-                setattr(cls, attr, decorator(getattr(cls, attr)))
-        return cls
-    return decorate
-
-# Exemple d'utilisation du décorateur avec une classe
-@for_all_methods(candle)
+@candle
 class MaClasse:
 
     def division(self, a, b):
-        print("DIVISION")
         return a / b
 
 
+ma_fonction()
 instance = MaClasse()
-print(type(instance))
 a = instance.division(1,0)
-print(a, "type : ", type(a))
