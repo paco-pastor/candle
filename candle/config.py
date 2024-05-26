dd80d3d3f176ad300d39654244df66f72f83f1d3
paco-pastor <pacopas1512@gmail.com>
2024-05-26 19:18:32 +0200

    🐛 Fix emoji crash on windows

--- candle/config.py
+++ candle/config.py
@@ -5,7 +5,7 @@ import pathlib
 
 class Config:
     ROOT_DIR = pathlib.Path(__file__).parent.resolve()
-    content = yaml.load(open(f"{ROOT_DIR}/candle.yaml"), Loader=yaml.FullLoader)
+    content = yaml.load(open(f"{ROOT_DIR}/candle.yaml", encoding="utf-8"), Loader=yaml.FullLoader)
 
     def get_emojis(self):
         return self.content.get("emojis")
